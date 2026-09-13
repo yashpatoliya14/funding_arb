53d65afb9cd15ff0fd01f32814b19b1381de27f5
yashpatoliya14 <yashpatoliya14@gmail.com>
2026-09-13 22:42:20 +0530

    update

--- exchanges/coinswitch_client.py
+++ exchanges/coinswitch_client.py
@@ -77,6 +77,7 @@ class CoinswitchClient(ExchangeClient):
             "X-AUTH-APIKEY": self.api_key,
             "X-AUTH-SIGNATURE": signature,
             "X-AUTH-EPOCH": epoch,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
         }
         return headers, decoded_path
 
